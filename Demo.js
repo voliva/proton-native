1bd97b689a2ebac2699c8543b17ad8b39f1fc2e1
Víctor Oliva <olivarra1@gmail.com>
2019-04-10 23:45:14 +0200

    Reworking renderer - Entry

--- Demo.js
+++ Demo.js
@@ -4,14 +4,29 @@ import { render, Window, App, TextInput, Dialog } from './src/';
 
 class Example extends Component {
   render() {
-    return (
-      <App>
-        <Window title="Test" size={{ w: 500, h: 500 }} margined={true}>
-          <TextInput stretchy={false} />
-        </Window>
-      </App>
-    );
+    return <TextInput />
   }
 }
 
-render(<Example />);
+import {
+	UiWindow,
+	UiMenu,
+	startLoop,
+	stopLoop,
+	onShouldQuit
+} from 'libui-node';
+
+const menu = new UiMenu('File');
+menu.appendQuitItem();
+
+const window = UiWindow('Initialization Example', 400, 300, true);
+
+onShouldQuit(() => {
+	window.close();
+	stopLoop();
+});
+
+window.show();
+startLoop();
+
+render(<Example />, window);
--- src/render/index.js
+++ src/render/index.js
@@ -5,7 +5,7 @@ import { connectDevtools } from '../devtools';
 export let ROOT_NODE = {};
 
 // Renders the input component
-function render(element) {
+function render_orig(element) {
   connectDevtools(DesktopRenderer);
 
   ROOT_NODE = createElement('ROOT');
@@ -17,6 +17,163 @@ function render(element) {
   // Schedules a top level update with current fiber and a priority level (depending upon the context)
   DesktopRenderer.updateContainer(element, node, null);
   //ROOT_NODE.render();
+}
+
+
+import * as LibUI from 'libui-node';
+
+const Reconciler = require('react-reconciler');
+const NewRenderer = Reconciler({
+  appendInitialChild(parentInstance, child) {
+    console.log(parentInstance, child);
+    throw 'appendInitialChild';
+  },
+
+  createInstance(
+    type,
+    props,
+    rootContainerInstance,
+    hostContext,
+    internalInstanceHandle,
+  ) {
+    console.log('createInstance', type, props, rootContainerInstance, hostContext);
+    if(typeof LibUI[type] !== 'function') {
+      throw new Error(`LibUI widget ${type} doesn't exist`);
+    }
+    return new LibUI[type]();
+  },
+
+  createTextInstance(text, rootContainerInstance, internalInstanceHandle) {
+    console.log(text, rootContainerInstance, internalInstanceHandle);
+    throw 'createTextInstance';
+  },
+
+  finalizeInitialChildren(
+    instance,
+    type,
+    props,
+    rootContainerInstance,
+    hostContext
+  ) {
+    console.log('finalizeInitialChildren', instance, type, props, rootContainerInstance, hostContext);
+    Object.entries(props).forEach(([key, value]) => {
+      if(typeof instance[key] !== 'undefined') {
+        instance[key] = value;
+      } else {
+        console.warn(`Element ${type} doesn't have prop ${key}`);
+      }
+    });
+    return false; // TODO
+  },
+
+  getPublicInstance(inst) {
+    console.log(inst);
+    throw 'getPublicInstance';
+  },
+
+  prepareForCommit(hostContext) {
+    console.log('prepareForCommit', hostContext);
+    // TODO
+  },
+
+  prepareUpdate(wordElement, type, oldProps, newProps) {
+    console.log(wordElement, type, oldProps, newProps);
+    throw 'prepareUpdate';
+  },
+
+  resetAfterCommit(hostContext) {
+    console.log('resetAfterCommit', hostContext);
+    // TODO
+  },
+
+  resetTextContent(wordElement) {
+    console.log(wordElement);
+    throw 'resetTextContent';
+  },
+
+  getRootHostContext(rootContainerInstance) {
+    // TODO
+    console.log('getRootHostContext', rootContainerInstance);
+    return {};
+  },
+
+  getChildHostContext(
+    parentHostContext,
+    type,
+    rootContainerInstance
+  ) {
+    console.log('getChildHostContext', parentHostContext, type, rootContainerInstance);
+    return parentHostContext;
+    // TODO;
+  },
+
+  shouldSetTextContent(type, props) {
+    console.log('shouldSetTextContext', type);
+    const textTypes = {
+      'ENTRY': true
+    };
+    return textTypes[type] || false;
+  },
+
+  now: () => new Date().getTime(),
+
+  useSyncScheduling: true,
+
+  // MUTATION
+
+  appendChild(parentInstance, child) {
+    console.log(parentInstance, child);
+    throw 'appendChild';
+  },
+
+  appendChildToContainer(container, child) {
+    console.log('appendChildToContainer', container, child);
+    if (container.appendChild) {
+      container.appendChild(child);
+    }else if(container.setChild) {
+      container.setChild(child);
+    } else {
+      throw new Error(`Can't append child to ${container.constructor.name}`);
+    }
+  },
+
+  removeChild(parentInstance, child) {
+    console.log(parentInstance, child);
+    throw 'removeChild';
+  },
+
+  removeChildFromContainer(parentInstance, child) {
+    console.log(parentInstance, child);
+    throw 'removeChildFromContainer';
+  },
+
+  insertBefore(parentInstance, child, beforeChild) {
+    console.log(parentInstance, child, beforeChild);
+    throw 'insertBefore';
+  },
+
+  commitUpdate(instance, updatePayload, type, oldProps, newProps) {
+    console.log(instance, updatePayload, type, oldProps, newProps);
+    throw 'commitUpdate';
+  },
+
+  commitMount(instance, updatePayload, type, oldProps, newProps) {
+    console.log(instance, updatePayload, type, oldProps, newProps);
+    throw 'commitMount';
+  },
+
+  commitTextUpdate(textInstance, oldText, newText) {
+    console.log(textInstance, oldText, newText);
+    throw 'commitTextUpdate';
+  },
+
+  supportsMutation: true,
+  supportsPersistence: false,
+});
+
+function render(element, window) {
+  const container = NewRenderer.createContainer(window);
+  NewRenderer.updateContainer(element, container, null);
 }
 
 export default render;
